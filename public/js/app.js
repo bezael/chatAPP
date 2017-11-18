9281db217eda4c47728edbeafd78f4344c302fbc
Bezael <bezael_e@hotmail.com>
2017-11-18 19:56:57 +0100

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,8 @@ let socket = io();
 socket.on('connect', function(){
 	console.log('Connected to socket.io server!');
 });
+
+socket.on('message', function(message){
+	console.log('New message:');
+	console.log(message);
+});
